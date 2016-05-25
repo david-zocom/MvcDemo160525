2ed6cd8508ed5711d52d833ba69aeadfd6502d61
David Andersson <david.andersson@alumni.chalmers.se>
2016-05-25 10:13:29 +0200

    initial

---
+++ MvcAjaxDemo/MvcAjaxDemo/Models/Djur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAjaxDemo.Models
+{
+    public class Djur
+    {
+        public int Id { get; set; }
+        public string Art { get; set; }
+        public int AntalBen { get; set; }
+
+    }
+}
---
+++ MvcAjaxDemo/MvcAjaxDemo/Models/ZooContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcAjaxDemo.Models
+{
+    public class ZooContext:DbContext
+    {
+        public ZooContext() : base("MvcAjaxDemo") { }
+
+        public DbSet<Djur> Djur { get; set; }
+
+    }
+}
